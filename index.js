7d560dfc2a6265061a18ab4dc04cba9a9f92d5c4
arRahat129 <rahashik129@gmail.com>
2026-07-31 16:32:34 +0600

    created backend with express, mongodb, cors, dotenv, and also placed mongodb connection

---
+++ index.js
@@ -0,0 +1,49 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: [
+        "http://localhost:5000",
+    ],
+    credentials: true
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Kinetix Server is running!');
+});
+
+const uri = process.env.MONGO_DB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Kinetix Server is running on port ${port}`);
+});
